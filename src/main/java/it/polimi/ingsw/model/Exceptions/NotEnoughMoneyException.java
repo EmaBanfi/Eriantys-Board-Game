50bece09494accf235cb1bca68ac74b5f76743b6
lucio_bencardino <lucio.bencardino@mail.polimi.it>
2022-04-13 21:02:00 +0200

    delete NotEnoughMoneyException

--- src/main/java/it/polimi/ingsw/model/Exceptions/NotEnoughMoneyException.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model.Exceptions;
-
-// if player doesn't have enough money when he buy a character card
-
-public class NotEnoughMoneyException extends Exception {
-    public NotEnoughMoneyException() {
-        super();
-    }
-}
